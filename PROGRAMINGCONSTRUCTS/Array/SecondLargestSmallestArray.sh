6ac1402bde2894baa8717286d41ee6df9947e539
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-05 10:14:47 +0530

    ADDED: Logic To Find Second Smallest Number In Array

--- PROGRAMINGCONSTRUCTS/Array/SecondLargestSmallestArray.sh
+++ PROGRAMINGCONSTRUCTS/Array/SecondLargestSmallestArray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-counter=0;
+#TO GENERATE RANDOM NUMBER
 for((i=0;i<=9;i++))
 do
 	a[i]=$((RANDOM%900+100))
@@ -8,20 +8,42 @@ done
 
 echo ${a[@]}
 
-min=0;
-max=0;
-#secmin=0;
-secmax=0;
+#TO FIND  SECOND MAX ELEMENT
+#VARIABLE INITIALIZATION
+max=${a[0]};
+secMax=${a[0]};
 
+#LOGIC TO FIND SECOND MAX
 for (( i=0; i<=9; i++ ))
 do
 	if [[ a[i] -gt $max ]]
 	then
-			secmax=$max;
-			max=${a[i]};
-	elif [[ a[i] -gt $secmax  &&  a[i] -lt $max ]]
+		secMax=$max;
+		max=${a[i]};
+	elif [[ a[i] -gt $secMax  &&  a[i] -lt $max ]]
 		then
-			secmax=${a[i]};
+			secMax=${a[i]};
 	fi
 done
-echo " $secmax ";
+echo "SECOND LARGEST ELEMENT IN ARRAY IS = $secMax ";
+
+
+#VARIABLE INITIALIZATION
+min=${a[0]};
+secMin=${a[0]};
+
+#TO FIND SECOND SMALLEST ELEMENT
+for (( i=0; i<=9; i++ ))
+do
+   if [[ a[i] -lt $min ]]
+   then
+      secMin=$min;
+      min=${a[i]};
+   elif [[ a[i] -lt $secMin  &&  a[i] -gt $min ]]
+   	then
+         secMin=${a[i]};
+   fi
+done
+echo "SECOND SMALLEST NUMBER IN ARRAY IS = $secMin"
+
+
