a4480bcdcf5b44f0ede2ecc8fac229cb9248cfd4
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-05 17:37:46 +0530

    ADDED:Array To Display Prime Factors

--- PROGRAMINGCONSTRUCTS/Array/PrimeFactorizationArray.sh
+++ PROGRAMINGCONSTRUCTS/Array/PrimeFactorizationArray.sh
@@ -3,14 +3,22 @@
 #INPUT FROM USER
 read -p "Enter Number : " NUM
 
+#TO FIND PRIME FACTORES
+#VARIABLE
+j=0;
 for (( i=2; i<=$NUM;  ))
 do
 	if [ $(($NUM%$i)) -eq 0 ]
 then
-		echo "$i"
+		#echo "$i"
+		a[j]=$i;
+		j=$(($j+1));
 		NUM=$(($NUM/$i))
 else
 		((i=$i+1))
 fi
 done
+
+#DISPLAYING ARRAY OF PRIME FACTORS
+echo "${a[@]}"
 
