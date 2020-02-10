0a59cab2a65bed14212f6efbb75eb60443e8ee3d
akshayssalunkhe <60338145+akshayssalunkhe@users.noreply.github.com>
2020-02-10 15:41:46 +0530

    MODIFIED: Corrected Echo Message And Indentation

--- PROGRAMINGCONSTRUCTS/SELECTIONSTATEMENT/IFSTATEMENT/DaysofMonthIf.sh
+++ PROGRAMINGCONSTRUCTS/SELECTIONSTATEMENT/IFSTATEMENT/DaysofMonthIf.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -x
+
+#USER INPUT FOR DAY AND MOUNTH
 read -p "enter the month" m
 read -p "enter day " d
 
 if [[ $m -ge 3 && $m -le 6 ]]
 then
 	if [[ $m -eq 3 && $d -ge 20 && $d -le 31 ]] ||
-     [[ $m -eq 4 && $d -ge 1 && $d -le 30 ]] ||
-	[[ $m -eq 5 && $d -ge 1 && $d -le 31 ]] ||
-		[[ $m -eq 6 && $m -ge 1 && $d -le 20 ]] 
+       [[ $m -eq 4 && $d -ge 1 && $d -le 30 ]] ||
+	   [[ $m -eq 5 && $d -ge 1 && $d -le 31 ]] ||
+	   [[ $m -eq 6 && $m -ge 1 && $d -le 20 ]] 
 		then
-		echo "heySXtrue"
+		echo "TRUE"
 		else
-		echo "false"
+		echo "FALSE"
     fi
 else
-	echo "false"
+	echo "FALSE"
 fi
