3457de49c12ad5b4dac4134a8090d3aa60f9affd
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-13 19:13:04 +0530

    MODIFIED:Proper Indentetion And Variable Name

--- PROGRAMINGCONSTRUCTS/selectionstatements/CASE/DisplayValueCase.sh
+++ PROGRAMINGCONSTRUCTS/selectionstatements/CASE/DisplayValueCase.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-#USER INPUT
-read -p "Enter the value: " Value
+#VARIABLE
+value=0;
 
-case $Value in
-	1) echo "One"
+#USER INPUT
+read -p "Enter the value: " value
+
+case $value in
+	1)
+		echo "One"
 		;;
-	10) echo "Ten"
+	10)
+		echo "Ten"
 		;;
-	100) echo "Hundred"
+	100)
+		echo "Hundred"
 		;;
-	1000) echo "Thousand"
+	1000)
+		echo "Thousand"
 		;;
-	10000) echo "Ten Thousand"
+	10000)
+		echo "Ten Thousand"
 		;;
-	100000) echo "Hundred Thousand"
+	100000)
+		echo "Hundred Thousand"
 		;;
 	*)
 		echo "Wrong Input"
