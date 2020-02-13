c71bd5a1c84424538a9643ae69710f0e136c0556
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-13 16:19:25 +0530

    MODIFIED:Proper Indentetion And Variable Name

--- PROGRAMINGCONSTRUCTS/forloop/PrimeNumberInRangeFor.sh
+++ PROGRAMINGCONSTRUCTS/forloop/PrimeNumberInRangeFor.sh
@@ -1,30 +1,34 @@
 #!/bin/bash -x
 
-#USER INPUT 
-read -p "Enter the Starting Number: " S
-if [ $S -eq 1 ]
+#VARIABLES
+start=0;
+end=0;
+isprime=0;
+
+#USER INPUT
+read -p "Enter the Starting Number: " start
+if [ $start -eq 1 ]
 then
-		S=$(( $S+1 ))
+	start=$(( $start+1 ))
 fi
-read -p "Enter the Ending Number: " E
+read -p "Enter the Ending Number: " end
 
 #CALCULATING PRIME NUMBERS BETWEEN RANGE
-for (( i=$S; i<=$E; i++ ))
+for (( index=$start; index<=$end; index++ ))
 do
 	isprime=1
-
-	for (( j=2; j<=$(($i/j)); j++ ))
+	for (( j=2; j<=$(($index/j)); j++ ))
 	do
-	if [ $(($i%j)) -eq 0 ]
-	then
-		isprime=$(($isprime-1))
-	break;
-	fi
+		if [ $(($index%j)) -eq 0 ]
+		then
+			isprime=$(($isprime-1))
+		break;
+		fi
 	done
 
-if [ $isprime -eq 1 ]
-then
-	echo "$i";
-fi
+	if [ $isprime -eq 1 ]
+	then
+		echo "$index";
+	fi
 done
 
