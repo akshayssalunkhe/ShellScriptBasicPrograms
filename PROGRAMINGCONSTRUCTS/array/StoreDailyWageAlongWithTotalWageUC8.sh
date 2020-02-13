076f005792a754f5f1137e45e429bbe42f7b8528
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-13 12:29:15 +0530

    MODIFIED:Added Proper Comments In Code

--- PROGRAMINGCONSTRUCTS/array/StoreDailyWageAlongWithTotalWageUC8.sh
+++ PROGRAMINGCONSTRUCTS/array/StoreDailyWageAlongWithTotalWageUC8.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
 #CONSTANTS FOR THE PROGRAM
-IS_PART_TIME=1;
-IS_FULL_TIME=2;
+IS_PART_TIME=2;
+IS_FULL_TIME=1;
 MAX_HRS_IN_MONTH=10;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
@@ -11,6 +11,7 @@ NUM_WORKING_DAYS=20;
 totalWorkHours=0;
 totalWorkingDays=0;
 
+#FUNCTION TO GET WORKING HOURS
 function getWorkingHours() {
 	case $1 in
 		$IS_FULL_TIME)
@@ -23,15 +24,17 @@ function getWorkingHours() {
 			workingHours=0
 			;;
 	esac
-	echo $workHours
+	echo $workingHours
 }
 
+#FUNCTION TO CALCULATE DAILY WAGE
 function calcDailyWage() {
 	local workHrs=$1
 	wage=$(( $workHours * $EMP_RATE_PER_HR ))
 	echo $wage
 }
 
+#CHECKING FOR CONDITIONS
 while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH &&
 			$totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
@@ -40,5 +43,7 @@ do
 	totalWorkHours=$($totalWorkHours+$workHours)
 	empDailyWage[$totalWorkingDays]="$( calcDailyWage * $workHours )"
 done
+
+#CALLING FUNCTION TO CALCULATE WAGE
 totalSalary="$( calcDailyWage $totalWorkHours )"
 echo "Daily Wage " ${empDailyWage[@]}
