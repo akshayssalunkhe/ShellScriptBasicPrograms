cdb143b685d3ee9e6c05913122ae3c2a35aa0036
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-14 12:29:07 +0530

    MODIFIED:Proper Indentetion And Variable Name

--- PROGRAMINGCONSTRUCTS/sequences/UnitConversion.sh
+++ PROGRAMINGCONSTRUCTS/sequences/UnitConversion.sh
@@ -1,27 +1,35 @@
 #!/bin/bash -x
 
+#VARIABLES
+feet=0;
+meter=0;
+totalSqureFeet=0;
+feetToMeter=0;
+totalSqureMeter=0;
+acres=0;
+
 #INCH TO FEET CONVERSION
-echo " inch to feet conversion"
 feet=$(echo "scale=2; 42*1/12" | bc );
 
 #AREA IN SQURE FEETS
-TotalSquareFeet=$((60*40))
-echo "TotalSquareFeet=$TotalSquareFeet "
+totalSquareFeet=$((60*40))
+echo "Total Square Feet=$totalSquareFeet "
 
-echo "feet to meter conversion"
-meter=$(echo "scale=2; 0.3048*60" | bc);
-echo "60 feet = $meter meters"
+#FEET TO METER CONVERSION
+feetToMeter=$(echo "scale=2; 0.3048*60" | bc);
+echo "60 Feet = $feetToMeter meters"
 
-meter1=$(echo "scale=2; 0.3048*40" | bc);
-echo "40 feet = $meter1 meters"
+meter=$(echo "scale=2; 0.3048*40" | bc);
+echo "40 Feet = $meter meters"
 
 #AREA IN SQURE METERS
-TotalSquareMeters=$(echo "scale=2; $TotalSquareFeet*0.092" | bc)
-echo "TotalSquareMeters= $TotalSquareMeters"
+totalSquareMeters=$(echo "scale=2; $totalSquareFeet*0.092" | bc)
+echo "Total Square Meters = $totalSquareMeters"
 
 #SQUREMETERS TO ACRE CONVERSION
-Acres=$( echo "scale=2; $TotalSquareMeters*0.00024" | bc);
+acres=$( echo "scale=2; $totalSquareMeters*0.00024" | bc);
 
 #AREA OF 25 PLOTS IN ACRES 
-AreaOf25PlotsInAcre=$( echo "scale=2; $Acres*25" | bc);
-echo "AreaOf25PlotsInAcres= $AreaOf25PlotsInAcre"
+areaOfPlotsInAcre=$( echo "scale=2; $acres*25" | bc);
+echo "Area Of 25 Plots In Acres= $areaOfPlotsInAcre"
+
