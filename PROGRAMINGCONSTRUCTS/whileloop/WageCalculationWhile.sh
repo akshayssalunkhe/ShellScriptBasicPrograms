eef56c7340266feacb01d6e86ef98fb5447760d6
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-14 14:54:07 +0530

    MODIFIED:Proper Indentetion And Variable Name

--- PROGRAMINGCONSTRUCTS/whileloop/WageCalculationWhile.sh
+++ PROGRAMINGCONSTRUCTS/whileloop/WageCalculationWhile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-#	CANSTANTS FOR PROGRAM
+#CANSTANTS FOR PROGRAM
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 MAX_HRS_IN_MONTH=160;
@@ -8,34 +8,35 @@ EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
 #VARIABLES
-totalEmpHrs=0;
+employeeCheck=0;
+totalEmployeeHrs=0;
 totalWorkingDays=0;
+employeeHrs=0;
 
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
-			$totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+#CHECKING FOR CONDITIONS
+while [[ $totalEmployeeHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-	empCheck=$((RANDOM%3));
-	case $empCheck in
+	employeeCheck=$((RANDOM%3));
+	case $employeeCheck in
 		$IS_FULL_TIME)
-			empHrs=8
-			;;
+			employeeHrs=8
+				;;
 		$IS_PART_TIME)
-			empHrs=4
-			;;
+			employeeHrs=4
+				;;
 		*)
-			empHrs=0
-			;;
-
+			employeeHrs=0
+				;;
 	esac
-		if [ $empHrs -ne 0 ]
-		then
-				((totalWorkingDays++));
-		fi
-	totalEmpHrs=$(($totalEmpHrs+$empHrs))
-
+	if [ $employeeHrs -ne 0 ]
+	then
+		((totalWorkingDays++));
+	fi
+	totalEmployeeHrs=$(($totalEmployeeHrs+$employeeHrs))
 done
 
-totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
+#CALCULATING TOTAL SALARY
+totalSalary=$(($totalEmployeeHrs*$EMP_RATE_PER_HR));
 
 
 
