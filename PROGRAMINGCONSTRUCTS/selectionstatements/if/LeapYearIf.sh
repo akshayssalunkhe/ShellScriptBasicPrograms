49f77d3fa4045e294a15db2f46f63666c9f1e390
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-14 10:22:21 +0530

    MODIFIED:Proper Indentetion And Variable Name

--- PROGRAMINGCONSTRUCTS/selectionstatements/if/LeapYearIf.sh
+++ PROGRAMINGCONSTRUCTS/selectionstatements/if/LeapYearIf.sh
@@ -1,17 +1,21 @@
 #!/bin/bash -x
 
-read -p"Enter four Digit Number " year;
-check=999;
-#zero=0;
-if [ $year -gt $check ]
+#VARIABLES
+year=0;
+start=999;
+end=10000;
+
+#INPUT FROM USER
+read -p "Enter Four Digit Number: " year;
+
+if [[ $year -gt $start && $year -lt $end ]]
 	then
  		if [[ $(($year%4)) -eq 0 ]] && [[ $(($year%100)) -ne 0 ]] || [[ $(($year%400)) -eq 0 ]]
-
 		then
-			echo "year is leap year"
+			echo "Year Is Leap Year"
 		else
-			echo "not a leap year"
-   	fi
+			echo "Not a Leap Year"
+		fi
 else
-	echo "enter right value"
-fi 
+	echo "Enter The Right Value"
+fi
