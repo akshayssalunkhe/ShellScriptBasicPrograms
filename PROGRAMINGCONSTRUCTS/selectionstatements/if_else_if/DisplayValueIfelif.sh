3c86ed250b385e76a8743a82eff631f1b21dcd52
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-14 11:06:22 +0530

    MODIFIED:Proper Indentetio And Variable Name

--- PROGRAMINGCONSTRUCTS/selectionstatements/if_else_if/DisplayValueIfelif.sh
+++ PROGRAMINGCONSTRUCTS/selectionstatements/if_else_if/DisplayValueIfelif.sh
@@ -1,25 +1,31 @@
 #!/bin/bash -x
-read -p" enter Number: " n
 
-if [ $n -eq 1 ]
+#VARIABLE
+number=0;
+
+#USER INPUT
+read -p" Enter Number: " number
+
+#CHECKING FOR CONDITION
+if [ $number -eq 1 ]
 then
-   echo "ONE"
-elif [ $n -eq 10 ]
+	echo "ONE"
+elif [ $number -eq 10 ]
 then
-    echo "TEN"
-elif [ $n -eq 100 ]
+	echo "TEN"
+elif [ $number -eq 100 ]
 then
-    echo "HUNDRED"
-elif [ $n -eq 1000 ]
+	echo "HUNDRED"
+elif [ $number -eq 1000 ]
 then
-    echo "THOUSAND"
-elif [ $n -eq 10000 ]
+	echo "THOUSAND"
+elif [ $number -eq 10000 ]
 then
-    echo "TEN THOUSAND"
-elif [ $n -eq 100000 ]
+	echo "TEN THOUSAND"
+elif [ $number -eq 100000 ]
 then
-    echo "HUNDRED THOUSAND"
+	echo "HUNDRED THOUSAND"
 else
-    echo "WRONG INPUT"
+	echo "WRONG INPUT"
 fi
 
